fa09bf874cb46a35ec8681f1aa5ddbc947284e03
Saiprasad Lokhande <lokhandesai07@gmail.com>
2024-02-25 23:02:08 +0530

    Initial Commit

---
+++ CryptoTrack/CryptoTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackApp.swift
+//  CryptoTrack
+//
+//  Created by Saiprasad lokhande on 25/02/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
