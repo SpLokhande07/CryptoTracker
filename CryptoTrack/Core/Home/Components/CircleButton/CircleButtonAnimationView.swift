3e42b337da65b78a622b7c141ad033fd6e75e8de
Saiprasad Lokhande <lokhandesai07@gmail.com>
2024-02-26 00:22:39 +0530

    done with header

---
+++ CryptoTrack/Core/Home/Components/CircleButton/CircleButtonAnimationView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleButtonAnimationView.swift
+//  CryptoTrack
+//
+//  Created by Saiprasad lokhande on 26/02/24.
+//
+
+import SwiftUI
+
+struct CircleButtonAnimationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleButtonAnimationView()
+}
