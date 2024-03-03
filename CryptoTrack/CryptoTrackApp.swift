dd3ad82f5c430bf7b7e7a578362098f9236944f2
Saiprasad Lokhande <lokhandesai07@gmail.com>
2024-03-03 22:58:59 +0530

    wokring on fetching data from api and updating UI

--- CryptoTrack/CryptoTrackApp.swift
+++ CryptoTrack/CryptoTrackApp.swift
@@ -9,13 +9,12 @@ import SwiftUI
 
 @main
 struct CryptoTrackApp: App {
+    @StateObject private var vm = HomeViewModel()
     var body: some Scene {
         WindowGroup {
-            
-            //            ContentView()
             NavigationView{
                 HomeView().navigationBarHidden(true)
-            }
+            }.environmentObject(vm)
         }
     }
 }
---
+++ CryptoTrack/Network/CoinDataService.swift
@@ -0,0 +1,26 @@
+//
+//  CoinDataService.swift
+//  CryptoTrack
+//
+//  Created by Saiprasad lokhande on 03/03/24.
+//
+
+import Foundation
+
+class CoinDataService {
+    @Published var allCoins : [CoinModel] = []
+    
+    init(){
+        getCoins()
+    }
+    
+    private func getCoins(){
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=24h&locale=en")
+        else { return }
+        
+        URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            
+    }
+    
+}
